5488091543a2dae0eaa5c37b241b411b1b6dc9a0
Nafeel483 <m.nafeelrasheed@gmail.com>
2023-06-05 13:12:42 +0500

    change server file code

--- server.js
+++ server.js
@@ -36,12 +36,12 @@ const PORT = process.env.PORT || 3000;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGOO_DB,
+    const conn = await mongoose.connect(`${process.env.MONGOO_DB}`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-        // useCreateIndex: false,
-        // useFindAndModify: false
+        useCreateIndex: false,
+        useFindAndModify: false
       },
     );
     console.log(`MongoDB Connected: ${conn.connection.host}`);
@@ -64,6 +64,6 @@ app.get("/", (req, res) => {
 //Connect to the database before listening
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log("listening for requests");
+    console.log(`listening on port ${PORT}`);
   })
 })
