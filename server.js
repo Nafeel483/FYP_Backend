ce8098cd4998e2c47fb53bb14912cc177415627f
Nafeel483 <m.nafeelrasheed@gmail.com>
2023-06-05 13:49:54 +0500

    revert server code

--- server.js
+++ server.js
@@ -5,53 +5,21 @@ const cors = require("cors");
 const user = require("./routes/user");
 const videos = require("./routes/videos");
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3008;
 
-// mongoose.connect(
-//   `${process.env.MONGOO_DB}`,
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     useCreateIndex: false,
-//     useFindAndModify: false
-//   },
-//   () => {
-//     console.log("connected to db");
-//   }
-// );
-
-// app.use(cors());
-// app.use(express.json());
-// //routes
-// app.use("/user", user);
-// app.use("/video", videos);
-// app.get("/", (req, res) => {
-//   res.send("Root place");
-// });
-
-// app.listen(port, () => {
-//   console.log(`listening on port ${port}`);
-// });
-
-
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGOO_DB,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: false,
-        useFindAndModify: false
-      },
-    );
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log("-----------Errot-----", error);
-    process.exit(1);
+mongoose.connect(
+  `${process.env.MONGOO_DB}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: false,
+    useFindAndModify: false
+  },
+  () => {
+    console.log("connected to db");
   }
-}
+);
 
-//Routes go here
 app.use(cors());
 app.use(express.json());
 //routes
@@ -61,9 +29,41 @@ app.get("/", (req, res) => {
   res.send("Root place");
 });
 
-//Connect to the database before listening
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
-  })
-})
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+
+// const connectDB = async () => {
+//   try {
+//     const conn = await mongoose.connect(process.env.MONGOO_DB,
+//       {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         useCreateIndex: false,
+//         useFindAndModify: false
+//       },
+//     );
+//     console.log(`MongoDB Connected: ${conn.connection.host}`);
+//   } catch (error) {
+//     console.log("-----------Errot-----", error);
+//     process.exit(1);
+//   }
+// }
+
+// //Routes go here
+// app.use(cors());
+// app.use(express.json());
+// //routes
+// app.use("/user", user);
+// app.use("/video", videos);
+// app.get("/", (req, res) => {
+//   res.send("Root place");
+// });
+
+// //Connect to the database before listening
+// connectDB().then(() => {
+//   app.listen(PORT, () => {
+//     console.log(`listening on port ${PORT}`);
+//   })
+// })
