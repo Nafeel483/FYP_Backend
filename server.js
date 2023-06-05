930f4ac13ce55af059e94845e8302f35fcb68c2d
Nafeel483 <m.nafeelrasheed@gmail.com>
2023-06-05 14:10:33 +0500

    add port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const user = require("./routes/user");
 const videos = require("./routes/videos");
 require("dotenv").config();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 mongoose.connect(
   `${process.env.MONGOO_DB}`,
